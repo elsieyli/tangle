72962305d0ec0c52e70e6b70eba0661e3dcd1db5
elsieyli <elsie.snowflake1@gmail.com>
2024-09-14 16:31:03 -0400

    no navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,13 @@
-
-import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom'; // Import BrowserRouter
-import Navbar from './Navbar';
-import Home from './home';  // Import Home or other components you need
+import React from "react";
+import Home from "./home";
 
 const App: React.FC = () => {
   return (
-    <Router> {/* Wrap your entire app in BrowserRouter */}
-      <Navbar /> {/* Navbar component */}
-      <Home /> {/* Other components such as Home, About, etc. */}
-    </Router>
+    <div className="App">
+      <Home />
+    </div>
   );
 };
 
 export default App;
+
