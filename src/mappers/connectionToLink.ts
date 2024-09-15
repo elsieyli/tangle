7b1c073674523840905b80cc0a94056bec64e991
Bokang Keith Mogomotsi <bigbk75@gmail.com>
2024-09-14 23:52:06 -0400

    fetching actual data

--- src/mappers/connectionToLink.ts
+++ src/mappers/connectionToLink.ts
@@ -1,5 +1,5 @@
 import { Doc, Id } from "../../convex/_generated/dataModel";
-import { Link } from "../components/data";
+import { GraphData, Link } from "../components/data";
 const connectionToLinks = (person: Doc<"people">, seen: Set<Id<"people">> ): Link[] => {
     const links = person.connected_to.map(p => {
         if (seen.has(p)) {
@@ -16,10 +16,20 @@ const connectionToLinks = (person: Doc<"people">, seen: Set<Id<"people">> ): Lin
 
 export const connectionsToLinks = (people: Doc<"people">[]) => {
     var seen: Set<Id<"people">> = new Set()
-    const links = people.map(p => {
+    const links = people.flatMap(p => {
         const link = connectionToLinks(p, seen)
         seen.add(p._id)
         return link
     })
     return links
+}
+
+export const peopleToGraphData = (people: Doc<"people">[]): GraphData => {
+    const links = connectionsToLinks(people)
+    const nodes = people.map(p => ({id: p._id}))
+
+    return {
+        nodes: nodes,
+        links: links,
+    }
 }
